1a254a734e88330ded51cc996efae7fe0041b6e6
TimOthry <timsonlee3@gmail.com>
2024-11-16 13:55:26 +0000

    Commit initial files

---
+++ src/main/java/cc/hackathon/shef/uk/cryptocrashouts/CryptoCrashoutsApplication.java
@@ -0,0 +1,13 @@
+package cc.hackathon.shef.uk.cryptocrashouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCrashoutsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoCrashoutsApplication.class, args);
+    }
+
+}
